2213237967fafcca9001795b5c2a38dab5c5779e
JuanValenciaOlvera <183111253@upmh.edu.mx>
2021-10-09 17:39:26 -0500

    v4

--- src/Data/card.js
+++ src/Data/card.js
@@ -1,28 +1,28 @@
 export const cardsInfo = [
     {
         id: 'Proyecto 1',
-        img: './assets/img/calculadora.png',
+        img: 'https://res.cloudinary.com/dicn2ueb2/image/upload/v1633818738/calculadora_x0go5s.png',
         titulo: 'Calculadora básica',
         descrip: 'Calculadora realizada con html,css y javascript para operaciones basicas y un modo oscuro que permanece gracias al localstorage',
         link: 'https://juanvalenciaolvera.github.io/Calculadora/'
     },
     {
         id: 'App Gifs',
-        img: './assets/img/gifs.png',
+        img: 'https://res.cloudinary.com/dicn2ueb2/image/upload/v1633818741/gifs_nld7yn.png',
         titulo: 'App Gifs',
         descrip: 'Aplicacion de gifs hecha con React y consumiendo la API de  Giphy, en la cual se pueden buscar gifs de cualquier cosa.',
         link: 'https://juanvalenciaolvera.github.io/react-gift-expert/'
     },
     {
         id: 'Local Storage',
-        img: './assets/img/lcoalstorage.png',
+        img: 'https://res.cloudinary.com/dicn2ueb2/image/upload/v1633818739/lcoalstorage_r6lh6d.png',
         titulo: 'Practica Local Storage',
         descrip: 'Practica de js puro repasando local Storage en el cual una serie de notas permanecen en base al local store y las mimas se pueden borrar una vez realizadas las notas.',
         link: 'https://elegant-shaw-8013c0.netlify.app/'
     },
     {
         id: 'Filtrado carros',
-        img: './assets/img/autos.png',
+        img: 'https://res.cloudinary.com/dicn2ueb2/image/upload/v1633818739/autos_leubkm.png',
         titulo: 'Filtrado de carros',
         descrip: 'Una practica en la cual se puede filtrar una lista de automóviles en tiempo real en base a distintos parámetros.',
         link: 'https://vigilant-feynman-4966ec.netlify.app/'
